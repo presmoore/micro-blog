fec2cb97f0e3ba5fb72b9f62ef9bf75df5500a69
Preston Moore <prestonmoore@me.com>
2015-07-01 15:17:31 -0400

    initial files, started on sign-in, sign-up, and sign-out, created user table

---
+++ app.rb
@@ -0,0 +1,55 @@
+require 'sinatra'
+require 'sinatra/activerecord'
+require './models.rb'
+
+set :database, "sqlite3:psblog.sqlite3"
+
+set :sessions, true
+
+
+
+def current_user
+    if session[:user_id]
+      @current_user = User.find(session[:user_id])
+    end
+end
+
+
+
+
+get "/" do
+  if current_user 
+    erb :home 
+  else
+    redirect "/sign_in"
+  end
+end
+
+get "/sign-in" do
+  erb :sign_in
+end
+
+post "/sign-in" do
+  @user = User.where(username: params[:user][:username]).first
+
+  if @user.password == params[:user][:password]
+    session[:user_id] = @user.id
+    redirect "/"
+  else
+    "Your username or password did not match"
+  end
+end
+
+post "/sign-up" do
+  confirmation = params[:confirm_password]
+
+  if confirmation == params[:user][:password]
+    @user = User.create(params[:user])
+    redirect "/"
+  else
+    "Your password & confirmation did not match, try again"
+  end
+end
+
+
+
